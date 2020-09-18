5ac33291913f08f276db7e3cb0e56508a2ad1b86
Michael <mihail.pateev@yandex.ru>
2020-09-18 16:24:12 +0300

    6Lesson not full

---
+++ Assets/Scripts/MainScene/DetectClicks.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectClicks : MonoBehaviour
+{
+    public GameObject buttons;
+    public Text playTxt, gameName;
+
+    public bool clicked; 
+
+    void OnMouseDown()
+    {
+        if (!clicked)
+        {
+            playTxt.gameObject.SetActive(false);
+            gameName.text = "0";
+
+            buttons.GetComponent<ScrollObject>().speed = -5f;
+            buttons.GetComponent<ScrollObject>().checkPost = -150f;
+        }
+    }
+}
