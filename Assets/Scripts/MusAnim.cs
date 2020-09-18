0f4d6dae5339289fb4733fb0804a2a5388578c9a
Michael <mihail.pateev@yandex.ru>
2020-09-18 12:29:06 +0300

    5Leson

---
+++ Assets/Scripts/MusAnim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MusAnim : MonoBehaviour
+{
+    private SpriteRenderer musicOn;
+    private float _movementSpeed = 0.1f;
+
+    void Start()
+    {
+        musicOn = GetComponent<SpriteRenderer>();
+        Destroy(gameObject, 5f);
+    }
+
+    void Update()
+    {
+        musicOn.color = new Color(musicOn.color.r, musicOn.color.g, musicOn.color.b, Mathf.PingPong(Time.time / 2.5f, 1.0f));
+
+        transform.position += transform.up * Time.deltaTime * _movementSpeed;
+    }
+}
